c69ed5dfb38ec8f50aaee1f536d938407f8fe54c
techcoderx <git@techcoderx.com>
2025-07-26 22:03:38 +0800

    import all sdks

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -6,3 +6,36 @@ import (
 
 //go:wasmimport sdk console.log
 func Log(s *string) *string
+
+//go:wasmimport sdk db.setObject
+func SetObject(key *string, value *string) *string
+
+//go:wasmimport sdk db.getObject
+func GetObject(key *string) *string
+
+//go:wasmimport sdk db.delObject
+func DelObject(key *string) *string
+
+//go:wasmimport sdk system.call
+func Call(method *string, payload *string) *string
+
+//go:wasmimport sdk system.getEnv
+func GetEnv(env *string) *string
+
+//go:wasmimport sdk crypto.sha256
+func Sha256(val *string) *string
+
+//go:wasmimport sdk crypto.ripemd160
+func Ripemd160(val *string) *string
+
+//go:wasmimport sdk hive.getbalance
+func GetBalance(account *string, asset *string) *string
+
+//go:wasmimport sdk hive.draw
+func Draw(amount *string, asset *string) *string
+
+//go:wasmimport sdk hive.transfer
+func Transfer(to *string, amount *string, asset *string) *string
+
+//go:wasmimport sdk hive.withdraw
+func Withdraw(to *string, amount *string, asset *string) *string
--- src/main.go
+++ src/main.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"contract-template/sdk"
+	"strings"
 )
 
 //go:wasmexport entrypoint
@@ -27,4 +28,133 @@ func Entrypoint(a *string) *string {
 	sdk.Log(a)
 	sdk.Log(&msg)
 	return a
+}
+
+//go:wasmexport setString
+func SetString(a *string) *string {
+	key := "myString"
+	sdk.SetObject(&key, a)
+	return a
+}
+
+//go:wasmexport getString
+func GetString(a *string) *string {
+	key := "myString"
+	return sdk.GetObject(&key)
+}
+
+//go:wasmexport clearString
+func ClearString(a *string) *string {
+	key := "myString"
+	return sdk.DelObject(&key)
+}
+
+//go:wasmexport dumpEnv
+func DumpEnv(a *string) *string {
+	varContractId := "contract_id"
+	varSender := "msg.sender"
+	varReqAuths := "msg.required_auths"
+	varReqPostingAuths := "msg.required_posting_auths"
+	varAnchrId := "anchor.id"
+	varAnchrBlk := "anchor.block"
+	varAnchrHeight := "anchor.height"
+	varAnchrTs := "anchor.timestamp"
+	varAnchrTxIdx := "anchor.tx_index"
+	varAnchrOpIdx := "anchor.op_index"
+	sdk.Log(sdk.GetEnv(&varContractId))
+	sdk.Log(sdk.GetEnv(&varSender))
+	sdk.Log(sdk.GetEnv(&varReqAuths))
+	sdk.Log(sdk.GetEnv(&varReqPostingAuths))
+	sdk.Log(sdk.GetEnv(&varAnchrId))
+	sdk.Log(sdk.GetEnv(&varAnchrBlk))
+	sdk.Log(sdk.GetEnv(&varAnchrHeight))
+	sdk.Log(sdk.GetEnv(&varAnchrTs))
+	sdk.Log(sdk.GetEnv(&varAnchrTxIdx))
+	sdk.Log(sdk.GetEnv(&varAnchrOpIdx))
+	ret := "Dump complete"
+	return &ret
+}
+
+//go:wasmexport sha256
+func Sha256(a *string) *string {
+	return sdk.Sha256(a)
+}
+
+//go:wasmexport ripemd160
+func Ripemd160(a *string) *string {
+	return sdk.Ripemd160(a)
+}
+
+//go:wasmexport getHiveBalance
+func GetHiveBalance(a *string) *string {
+	asset := "hive"
+	return sdk.GetBalance(a, &asset)
+}
+
+//go:wasmexport getHbdBalance
+func GetHbdBalance(a *string) *string {
+	asset := "hbd"
+	return sdk.GetBalance(a, &asset)
+}
+
+//go:wasmexport getHiveConsBalance
+func GetHiveConsBalance(a *string) *string {
+	asset := "hive_consensus"
+	return sdk.GetBalance(a, &asset)
+}
+
+//go:wasmexport drawHive
+func DrawHive(a *string) *string {
+	asset := "hive"
+	return sdk.Draw(a, &asset)
+}
+
+//go:wasmexport drawHbd
+func DrawHbd(a *string) *string {
+	asset := "hbd"
+	return sdk.Draw(a, &asset)
+}
+
+//go:wasmexport transferHive
+func TransferHive(a *string) *string {
+	asset := "hive"
+	params := strings.Split((*a), ",")
+	if len(params) < 2 {
+		err := "invalid payload"
+		return &err
+	}
+	return sdk.Transfer(&params[0], &params[1], &asset)
+}
+
+//go:wasmexport transferHbd
+func TransferHbd(a *string) *string {
+	asset := "hbd"
+	params := strings.Split((*a), ",")
+	if len(params) < 2 {
+		err := "invalid payload"
+		return &err
+	}
+	return sdk.Transfer(&params[0], &params[1], &asset)
+}
+
+//go:wasmexport withdrawHive
+func WithdrawHive(a *string) *string {
+	asset := "hive"
+	params := strings.Split((*a), ",")
+	if len(params) < 2 {
+		err := "invalid payload"
+		return &err
+	}
+	return sdk.Withdraw(&params[0], &params[1], &asset)
+}
+
+//go:wasmexport withdrawHbd
+func WithdrawHbd(a *string) *string {
+	asset := "hbd"
+	params := strings.Split((*a), ",")
+	if len(params) < 2 {
+		err := "invalid payload"
+		return &err
+	}
+	return sdk.Withdraw(&params[0], &params[1], &asset)
 }
